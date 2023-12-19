e14ec9b8ae01c9dca99b471761f88c33a93f33e7
Jake shim <shjun1411@naver.com>
2023-12-19 23:34:09 +0900

    add multiple webpack config files

---
+++ webpack/webpack.common.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+// 진입점 파일 크기가 244kb를 초과하는 경우 코드스플리팅 권장 alert 해제 
+  performance : {
+    hints : false
+  },
+  module: { 
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      // css관련 웹팩 설정
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
+  stats: 'errors-only',
+}
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,52 +1,14 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// for merging  webpack.common.js + each 'dev'|'prod' 
 
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-// 진입점 파일 크기가 244kb를 초과하는 경우 코드스플리팅 권장 alert 해제 
-  performance : {
-    hints : false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-      // css관련 웹팩 설정
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  //process.env.node_env를 어떤 모드 기준으로 할 것인지
-  mode: 'development', 
-  // mode: 'production', 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-  ],
-  stats: 'errors-only',
+
+const {merge} = require('webpack-merge')
+const commonConfig = require('./webpack.common.js');
+
+// package.json 파일에서 전달하는 환경 변수에 엑세스할 수 있음.
+// 객체가 아닌 함수 형태로 할당
+module.exports = (envVars)=>{
+  const { env } = envVars // destructuring
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig) // 두 config 파일을 병합
+  return config 
 }
