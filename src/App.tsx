36218b2e3c35824a6bf7b7f4a0855d92a96f1399
Jake shim <shjun1411@naver.com>
2023-12-19 18:55:30 +0900

    add svg rendering config

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import './styles.css';
 import WebpackImg from './webpackImg.jpeg';
+import Profile from './defaultProfile.svg';
 
 const App = () => {
   return (
     <>
     <h1>React Typescript Webpack Starter Template</h1>
     <img src={WebpackImg} alt="imgs" width="400" height="300"></img>
+    <img src={Profile} alt="imgs" width="400" height="300"></img>
     </>
   )
 }
